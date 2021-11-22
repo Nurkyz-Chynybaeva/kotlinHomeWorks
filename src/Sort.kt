6edd5475432fa94239fc1f93f2db37d19cb53ad0
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-22 12:52:36 +0600

    third home owrk

---
+++ src/Sort.kt
@@ -0,0 +1,27 @@
+fun sort(sampleArray:IntArray){
+    var n=sampleArray.size
+    var temp:Int
+    for(i in 0..n-1){
+        var indexOfMin = i
+        for(j in n-1 downTo  i){
+            if(sampleArray[j]< sampleArray[indexOfMin])
+                indexOfMin=j
+        }
+        if(i!=indexOfMin){
+            temp = sampleArray[i]
+            sampleArray[i]= sampleArray[indexOfMin]
+            sampleArray[indexOfMin]=temp
+        }
+    }
+}
+
+fun main(arg: Array<String>) {
+    println("Before Sort")
+    var A = intArrayOf(1, 7, 3, 19, 9, 4, 8, 2)
+    for (i in A) print(i)
+
+    sort(A)
+
+    println("Sorted array is : ")
+        for (i in A) print(i)
+}
